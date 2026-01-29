0aea4e8537a9c8fd8664dc361f7efcf7f5715ceb
Jonathan <jonathanmalheiro80065@gmail.com>
2026-01-29 15:22:07 -0300

    primeiro commit

---
+++ N1_NC5_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    int idade = 25;
+    float altura = 1.75;
+    char opcao = 'S';
+    char nome[20] = "Sergio";
+ 
+    printf("A idade do %s é: %d\n", nome, idade);
+    printf("A altuira é: %e\n", altura);
+    printf("A opcao é :%c\n", opcao);
+    
+    
+ 
+    return 0;
+}
---
+++ n1_nc4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3
+    char letra 'A';
+    char nome[20] = "Jonathan";
+
+    idade = 25;
+
+    quantidade = 10;
+   
+}
